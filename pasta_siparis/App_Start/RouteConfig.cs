dd76e6c434340b20a16bf9cceca1650ad9168212
Doğan KARTAL <dogan@dogan-kartal.com>
2018-10-10 18:35:45 +0300

    Default Index And Page Title Update

--- pasta_siparis/App_Start/RouteConfig.cs
+++ pasta_siparis/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace pasta_siparis
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
